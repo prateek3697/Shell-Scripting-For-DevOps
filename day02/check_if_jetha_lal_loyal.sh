4d3193652ec5bb63a93403f949977921e89d2012
Ubuntu <ubuntu@ip-172-31-10-212.ap-south-1.compute.internal>
2024-12-15 12:06:07 +0000

    New Update

---
+++ day02/check_if_jetha_lal_loyal.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+
+<<Disclaimer
+
+This is just for infotaiment purpose
+
+Disclaimer
+
+read -p "Jetha ne mud ke kise dekha: " bandi
+
+read -p "Jetha ka pyaar % : " pyaar
+
+if [[ $bandi == "daya bhabhi" ]];
+
+then 
+	echo "Jetha loyal hai"
+
+
+elif [[ $pyaar -ge 100 ]];
+
+then 
+	echo "Jetha loyal hai"	
+
+
+else
+	echo "jetha hrami hai"
+
+fi	
---
+++ day02/for_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# This is for & while loops
+
+<< comment
+
+ 1 is argument 1 which is folder name.
+ 2 is start range
+ 3 is end range 
+
+comment
+
+for(( num=$2 ; num<=$3 ; num++ ))
+
+
+do
+	mkdir "$1$num"
+done
